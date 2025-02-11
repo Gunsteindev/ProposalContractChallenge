961f2f449695fcbb60366769b9deac4993c1354a
Gunsteindev <gunstein.dev@gmail.com>
2025-02-11 16:13:50 +0000

    Update the create function with the title parameter

--- proposal.sol
+++ proposal.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.18;
 
 contract ProposalContract {
+    uint256 private counter;
     struct Proposal {
         string description; 
         string title;
@@ -14,4 +15,9 @@ contract ProposalContract {
     }
 
     mapping(uint256 => Proposal) proposal_history;
+
+    function create(string calldata _description, string memory _title,  uint256 _total_vote_to_end) external {
+        count += 1;
+        proposal_history[counter] = Proposal(_description, _title, 0, 0, 0, _total_vote_to_end, false, true);
+    }
 }
